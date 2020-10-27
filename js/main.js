061fccb16691203e8337026755ca4ac18c2e1bfb
grand-kristina <kristina.yarlykova@mail.ru>
2020-10-27 22:50:47 +0700

    Добавила JS

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var openerElem = document.querySelector('.button-search'); 
+var modalElem = document.querySelector('.offer'); 
+
+if (openerElem) { 
+openerElem.addEventListener('click', function (evt) { 
+evt.preventDefault(); 
+modalElem.classList.toggle('offer-open'); 
+}); 
+}
