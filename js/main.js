f76597eb077385bbbfcd823eac1b34f1c8de5bd2
grand-kristina <kristina.yarlykova@mail.ru>
2020-10-28 21:17:33 +0700

    Доработки 28.10

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var openerElem = document.querySelector('.button-search'); 
-var modalElem = document.querySelector('.offer'); 
+var openerElem = document.querySelector('.button-search');
+var modalElem = document.querySelector('.offer');
 
-if (openerElem) { 
-openerElem.addEventListener('click', function (evt) { 
-evt.preventDefault(); 
-modalElem.classList.toggle('offer-open'); 
-}); 
+if (openerElem) {
+    openerElem.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        modalElem.classList.toggle('offer-open');
+    });
 }
